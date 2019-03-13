0253e2b12ff73ff547494cc3e75119c9ef3391e7
daaviidmartinez <44229292+daaviidmartinez@users.noreply.github.com>
2019-03-13 09:50:35 +0100

    Add files via upload
    Actividad 7

---
+++ registro.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="listadoUsuarios.php" method="post">
+        <label for="id">Id</label>
+        <br>
+        <input type="text" name="id" id="id" value="">
+        <br><br>
+        <label for="nombre">Nombre</label>
+        <br>
+        <input type="text" name="nombre" id="nombre" value="">
+        <br><br>
+        <label for="apellidos">Apellidos</label>
+        <br>
+        <input type="text" name="apellidos" id="apellidos" value="">
+        <br><br>
+        <label for="curso">Curso</label>
+        <br>
+        <input type="text" name="curso" id="curso" value="">
+        <br><br>
+        <input type="submit" value="ENVIAR">
+  </body>
+</html>
